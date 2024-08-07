889e3f50033fab55b74a5e2a971abd64d3d6864e
softbunnyware <ziga.serafin@soft-bunnyware.com>
2024-08-08 00:34:48 +0200

    Controller configuration

---
+++ Src/API/Server/Controllers/Base/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers.Base;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseController : ControllerBase
+{
+}
---
+++ Src/API/Server/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Controllers.Base;
+
+namespace Server.Controllers;
+
+public class MoviesController : BaseController
+{
+    [HttpGet("{id:guid}")]
+    public async Task GetAsync(Guid id)
+    {
+    }
+
+    [HttpGet("list")]
+    public async Task GetListAsync()
+    {
+    }
+
+    [HttpPost("create")]
+    public async Task CreateAsync()
+    {
+    }
+
+    [HttpPut("update")]
+    public async Task UpdateAsync()
+    {
+    }
+
+    [HttpDelete("delete/{id:guid}")]
+    public async Task DeleteAsync(Guid id)
+    {
+    }
+}

cf2928e01ec73de174e7d0e454014fb54bd13069
softbunnyware <ziga.serafin@soft-bunnyware.com>
2024-08-07 20:27:23 +0200

    Added Domain base for enetities

---
+++ Src/API/Core/Domain/Abstractions/IEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Abstractions;
+
+public interface IEntity<TId> : IEntity
+{
+    TId Id { get; }
+}
+
+public interface IEntity
+{
+}
---
+++ Src/API/Core/Domain/Base/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Abstractions;
+
+namespace Domain.Base;
+
+public abstract class BaseEntity : BaseEntity<Guid>
+{
+}
+
+public abstract class BaseEntity<TId> : IEntity<TId>
+{
+    public TId Id { get; protected set; } = default!;
+}
